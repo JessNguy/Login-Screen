622b1d923429371c40a7d4b47de1c5ee9dc776a0
JessNguy <jessnguy642@ed.amdsb.ca>
2016-02-12 09:36:14 -0500

    added header comment

--- Login Screen/Form1.cs
+++ Login Screen/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//created by Jess Nguyen
+// Feb 12, 2016
 namespace Login_Screen
 {
     public partial class Form1 : Form
